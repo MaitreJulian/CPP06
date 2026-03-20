4103b9ef0cd8bb3a41bab38d9eb87a350e97add5
MaitreJulian <julian.venka@outlook.be>
2026-03-20 23:16:32 +0100

    Add files via upload

---
+++ ex00/ScalarConverter.hpp
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ScalarConverter.hpp                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: julian <julian@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/03/18 12:31:26 by julian            #+#    #+#             */
+/*   Updated: 2026/03/18 12:31:55 by julian           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SCALARCONVERTER_HPP
+#define SCALARCONVERTER_HPP
+
+#include <iostream>
+#include <string>
+
+class ScalarConverter
+{
+    public:
+        static void convert(const std::string& literal);
+};
+
+#endif
